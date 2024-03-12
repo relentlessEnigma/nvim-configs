d82c7e3add69c66d51a7c0118a1a64d8aca52ee1
Tiago Esteves (CTW) <tiago.esteves@criticaltechworks.com>
2024-03-12 18:13:45 +0000

    Added new files

--- after/plugin/mason.lua
+++
@@ -1,6 +0,0 @@
-require("mason").setup()
-require("mason-lspconfig").setup({
-    ensure_installed = { "tsserver" },
-    -- ensure_installed = { "tsserver", "lua_ls", "jdtls", "angularls", "bashls"},
-    automatic_installation = true,
-})
--- lua/kori91/init.lua
+++
@@ -1,3 +0,0 @@
-require("kori91.remap")
-require("kori91.packer")
-require("kori91.set")
